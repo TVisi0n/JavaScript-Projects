2d6c1649c562c93fa89a72c129f0d5a561151e72
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-08-14 17:37:16 -0700

    Basic javascript project submissin

--- Basic JavaScript Projects/project1_experessions_alert/JS/main.js
+++ Basic JavaScript Projects/project1_experessions_alert/JS/main.js
@@ -1,1 +1,6 @@
-doc("Hello World");
+var Sent1 = "This is the beginning of the string" // Assigning 2 variables
+var Sent2 = " and this is the end of the string."
+
+document.write(Sent1 + Sent2) // Using doc.write method and calling the 2 variables I set above and concatenating them
+
+window.alert(3 + 4) // Expression example
