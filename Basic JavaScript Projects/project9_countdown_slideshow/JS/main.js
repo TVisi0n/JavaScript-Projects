f6be53ee470f2614d409f4a1d862d04076b7a0f7
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-08-16 18:25:54 -0700

    Created project 9

---
+++ Basic JavaScript Projects/project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,9 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").ariaValueMax
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout 
+    }
+}
