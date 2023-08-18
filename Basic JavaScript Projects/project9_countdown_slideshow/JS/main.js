587f4aa1e14964190b5fa0dc28fc11df62fc7eee
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-08-17 19:06:04 -0700

    tried to troubleshoot issue

--- Basic JavaScript Projects/project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/project9_countdown_slideshow/JS/main.js
@@ -14,7 +14,7 @@ function countdown() {
     tick();
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -23,20 +23,20 @@ function plusSlides(n) {
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
-  }
+}  
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementById("mySlides");
-    let dots = document.getElementById("dot");
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slide.length}
+    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-}
+  }
